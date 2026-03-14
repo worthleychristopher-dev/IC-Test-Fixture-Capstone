61b7b76a8d635f67165ee8bd398c07989ecea327
qdiep1 <qingping.diep12@gmail.com>
2026-03-14 11:21:40 -0400

    refactored unit tests to reflect refactor in source code

--- python_src/tests/test_testvector.py
+++ python_src/tests/test_testvector.py
@@ -1,61 +1,61 @@
 import pytest
-from ICTestFixture import testvector
+from ICTestFixture.core import testvector
 
 @pytest.fixture
-def reset_testvector():
+def resetTestvector():
     """
         set all class attributes of TestVector None before every and after every test
     """
     # clear before test
-    testvector.TestVector.update_global_params(None)
-    testvector.TestVector.update_pin_map(None)
+    testvector.TestVector.updateGlobalParams(None)
+    testvector.TestVector.updatePinMap(None)
     yield
     # clear after test
-    testvector.TestVector.update_global_params(None)
-    testvector.TestVector.update_pin_map(None)
+    testvector.TestVector.updateGlobalParams(None)
+    testvector.TestVector.updatePinMap(None)
 
 # updates TestVector class attributes for tests when called
 @pytest.fixture
-def set_testvector_pin_map(reset_testvector):
-    testvector.TestVector.update_pin_map({"A": 3, "Y": 8})
+def setTestvectorPinMap(resetTestvector):
+    testvector.TestVector.updatePinMap({"A": 3, "Y": 8})
 
 @pytest.fixture
-def set_testvector_voltage(reset_testvector):
-    testvector.TestVector.update_global_params({"VCC Voltage": "5V"})
+def setTestvectorVoltage(resetTestvector):
+    testvector.TestVector.updateGlobalParams({"VCC Voltage": ["5V"]})
 
 @pytest.fixture
-def set_testvector_thresholds(reset_testvector):
-    testvector.TestVector.update_global_params({"Output Low": 0.3, "Output High": 3.4})
+def setTestvectorThresholds(resetTestvector):
+    testvector.TestVector.updateGlobalParams({"Output Low": [0.3], "Output High": [3.4]})
 
 
-def test_testvector_IOCommand():
-    io_cmd  = testvector.IOCommand([1,3,2], ["H"], "3.3V", testvector.LogicMapping.map)
+def testTestvectorIOCommand():
+    ioCmd  = testvector.IOCommand([1,3,2], ["H"], "3.3V", testvector.LogicMapping.Map)
     # test by numeric index
-    assert io_cmd[0] == [1,3,2]
-    assert io_cmd[1] == ["H"]
-    assert io_cmd[2] == "3.3V"
-    assert io_cmd[3] == testvector.LogicMapping.map
+    assert ioCmd[0] == [1,3,2]
+    assert ioCmd[1] == ["H"]
+    assert ioCmd[2] == "3.3V"
+    assert ioCmd[3] == testvector.LogicMapping.Map
 
     # test by name index
-    assert io_cmd.pins == [1,3,2]
-    assert io_cmd.pin_vals == ["H"]
-    assert io_cmd.volt_type == "3.3V"
-    assert io_cmd.cmd_type == testvector.LogicMapping.map
+    assert ioCmd.pins == [1,3,2]
+    assert ioCmd.pinVals == ["H"]
+    assert ioCmd.voltType == "3.3V"
+    assert ioCmd.cmdType == testvector.LogicMapping.Map
 
-def test_testvector_classattr(reset_testvector):
+def testTestvectorClassattr(resetTestvector):
     # initially None at first
-    assert testvector.TestVector.global_params == None
-    assert testvector.TestVector.pin_map == None
+    assert testvector.TestVector.globalParams == None
+    assert testvector.TestVector.pinMap == None
 
-    testvector.TestVector.update_global_params({"VCC Voltage": "5V", "Output Low": 2.8})
-    testvector.TestVector.update_pin_map({"A": 5, "CLK": 20})
-    assert testvector.TestVector.global_params == {"VCC Voltage": "5V", "Output Low": 2.8}
-    assert testvector.TestVector.pin_map == {"A": 5, "CLK": 20}
+    testvector.TestVector.updateGlobalParams({"VCC Voltage": "5V", "Output Low": 2.8})
+    testvector.TestVector.updatePinMap({"A": 5, "CLK": 20})
+    assert testvector.TestVector.globalParams == {"VCC Voltage": "5V", "Output Low": 2.8}
+    assert testvector.TestVector.pinMap == {"A": 5, "CLK": 20}
 
-    testvector.TestVector.update_global_params({"Output High": 3.4, "VCC Pin": 6})
-    testvector.TestVector.update_pin_map({"Q": 2, "QNot": 1})
-    assert testvector.TestVector.global_params == {"Output High": 3.4, "VCC Pin": 6}
-    assert testvector.TestVector.pin_map == {"Q": 2, "QNot": 1}
+    testvector.TestVector.updateGlobalParams({"Output High": 3.4, "VCC Pin": 6})
+    testvector.TestVector.updatePinMap({"Q": 2, "QNot": 1})
+    assert testvector.TestVector.globalParams == {"Output High": 3.4, "VCC Pin": 6}
+    assert testvector.TestVector.pinMap == {"Q": 2, "QNot": 1}
 
 @pytest.mark.parametrize(
     "pin, expected",
@@ -65,11 +65,11 @@ def test_testvector_classattr(reset_testvector):
         ("Y", 8) # pin id input 2
     ]
 )
-def test_testvector_get_pin(set_testvector_pin_map, pin, expected):
-    assert testvector.TestVector.get_pin(pin) == expected
+def testTestvectorGetPin(setTestvectorPinMap, pin, expected):
+    assert testvector.TestVector.getPin(pin) == expected
 
 @pytest.mark.parametrize(
-    "logic, volt_type, expected",
+    "logic, voltType, expected",
     [
         (0, "2.5V", 0), # Logic 0
         ("L", "1.8V", 0), # Logic L
@@ -78,11 +78,11 @@ def test_testvector_get_pin(set_testvector_pin_map, pin, expected):
         (1, None, "5V") # default to VCC Voltage
     ]
 )
-def test_testvector_get_voltage(set_testvector_voltage, logic, volt_type, expected):
-    assert testvector.TestVector.get_voltage(logic, volt_type) == expected
+def testTestvectorGetVoltage(setTestvectorVoltage, logic, voltType, expected):
+    assert testvector.TestVector.getVoltage(logic, voltType, paramIdx=0) == expected
     
 @pytest.mark.parametrize(
-    "adc_val, isInt, expected",
+    "adcVal, isInt, expected",
     [
         (0.21, False, "L"), # below low threshold, not int
         (0.21, True, 0), # below low threshold, int
@@ -92,5 +92,5 @@ def test_testvector_get_voltage(set_testvector_voltage, logic, volt_type, expect
         (4.2, True, 1) # above high threshold, int
     ]
 )
-def test_testvector_logic_from_thld(set_testvector_thresholds, adc_val, isInt, expected):
-    assert testvector.TestVector.logic_from_thld(adc_val, isInt) == expected
+def testTestvectorLogicFromThld(setTestvectorThresholds, adcVal, isInt, expected):
+    assert testvector.TestVector.logicFromThld(adcVal, isInt, paramIdx=0) == expected
